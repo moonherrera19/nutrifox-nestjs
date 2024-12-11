3345e01b7df24d1eca7fbce49c6618a28d3ba645
CHGL17 <L21010280@zamora.tecnm.mx>
2024-12-11 04:01:25 -0600

    Update ormconfig.ts

--- ormconfig.ts
+++ ormconfig.ts
@@ -2,10 +2,10 @@ import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: 'localhost', // Reemplaza con la dirección de tu servidor
+  host: 'mysql-nutrifox-accitesz-207d.i.aivencloud.com', // Reemplaza con la dirección de tu servidor
   port: 3306,
-  username: 'root', // Reemplaza con tu usuario de la base de datos
-  password: '12345', // Reemplaza con tu contraseña
+  username: 'avnadmin', // Reemplaza con tu usuario de la base de datos
+  password: 'AVNS_uycO9Kf56CcxocEgdUx', // Reemplaza con tu contraseña
   database: 'nutrifox',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: true, // Solo para desarrollo, NO en producción,
